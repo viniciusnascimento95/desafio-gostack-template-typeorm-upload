831d942a1a2685d48b8d9b6a0ceb4fd71b7320ba
nascimento <nascimento.vinicius32@gmail.com>
2020-05-30 21:36:25 -0400

    update repor routes models repository

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    // Validar se esiste id
+
+    const transactionRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
